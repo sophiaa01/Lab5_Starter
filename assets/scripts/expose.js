2c7af317ba708ca052f47c18bef643aa455b10e4
Andrew Le <latuan.nm@gmail.com>
2026-05-04 14:49:38 -0700

    Change expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,37 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
+function init() 
+{
   // TODO
+  const image = document.querySelector("#expose img");
+  const sound = document.querySelector("#expose audio");
+  const button = document.querySelector("#expose button");
+
+  let dropDownList = document.querySelector("#expose select");
+
+
+  dropDownList.addEventListener('change', function() 
+  {
+    if (event.target.value == "air-horn")
+    {
+      image.src = "assets/images/air-horn.svg";
+      sound.src = "assets/audio/air-horn.mp3";
+    }
+    else if (event.target.value == "car-horn")
+    {
+      image.src = "assets/images/car-horn.svg";
+      sound.src = "assets/audio/car-horn.mp3";
+    }
+    else if (event.target.value == "party-horn")
+    { 
+      image.src = "assets/images/party-horn.svg";
+      sound.src = "assets/audio/party-horn.mp3";
+    }
+  });
+  
+  button.addEventListener('click', function() 
+  {
+    sound.play();
+  });
 }
