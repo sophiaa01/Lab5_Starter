2547b71a0c452ea53c66d8d70e354ef326f62dd8
Andrew Le <latuan.nm@gmail.com>
2026-05-04 23:12:48 -0700

    Complete unit.test

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,18 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+});
+
+test('adds 2 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+});
+
+test('adds 2 + 2 to equal 4', () => {
+    expect(1 + 2).toBe(4);
+});
+
+test('adds 1 + 2 to equal 4', () => {
+    expect(1 + 2).toBe(4);
+});
