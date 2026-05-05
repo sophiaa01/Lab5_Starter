64f9381bd03f85a28e9703ff0808f4a582070b5f
Andrew Le <latuan.nm@gmail.com>
2026-05-05 12:19:34 -0700

    Finish Part 1 of Explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,54 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
+function init() 
+{
   // TODO
+  const voiceSelect = document.getElementById('voice-select');
+  const textInput = document.getElementById('text-to-speak');
+  const speakButton = document.querySelector('#explore button');
+  const img = document.querySelector('#explore img');
+
+  let voices = [];
+
+  speakButton.addEventListener('click', () => 
+  {
+    if (voiceSelect.value === 'select')
+    {
+      return;
+    }
+
+    const text = textInput.value;
+    let utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = voices[voiceSelect.selectedIndex - 1];
+
+    utterance.onstart = () => 
+    {
+      img.src = 'assets/images/smiling-open.png';
+    };
+
+  utterance.onend = () => 
+  {
+    img.src = 'assets/images/smiling.png';
+  };
+
+    speechSynthesis.cancel();
+    speechSynthesis.speak(utterance);
+  });
+
+  function loadVoices()
+  {
+    voices = speechSynthesis.getVoices();
+    
+    for(let i in voices)
+    {
+      const option = document.createElement('option');
+      option.value = voices[i].name;
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  speechSynthesis.addEventListener('voiceschanged', loadVoices);
+  loadVoices();
 }
